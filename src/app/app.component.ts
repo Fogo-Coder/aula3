3711959e9999f64e93100ba2b3e5ae9755bf9f47
FogoDaniel <carroandrade139@gmail.com>
2025-02-26 22:06:47 -0300

    aula3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuSuperiorComponent } from "./menu-superior/menu-superior.component";
+import { MediaSimplesComponent } from './media-simples/media-simples.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuSuperiorComponent, MediaSimplesComponent, CalculadoraComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
